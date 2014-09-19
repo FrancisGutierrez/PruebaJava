4e200b67a9682176001d00af176b9093d7fb2769
OriolUtbl <oriol.utrillas@gmail.com>
2014-09-19 18:19:13 +0200

    cambiado again

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 	public static void main (String [] args){
 		System.out.println("HELLO");
 		System.out.println("First change");
+		System.out.println("second change");
+
 	}
 }
